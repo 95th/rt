dc18f0b987b86df925ea029f3a667b381752069f
Gurwinder Singh <gurwindersi@adobe.com>
2026-01-20 07:40:12 +0530

    Final scene setup

--- src/interval.rs
+++ src/interval.rs
@@ -7,23 +7,12 @@ pub struct Interval {
 }
 
 impl Interval {
-    pub const EMPTY: Self = Self::new(f64::INFINITY, -f64::INFINITY);
-    pub const UNIVERSE: Self = Self::new(-f64::INFINITY, f64::INFINITY);
-
     pub const fn new(min: f64, max: f64) -> Self {
         Self { min, max }
     }
 
     pub fn with_max(self, max: f64) -> Self {
         Self { min: self.min, max }
-    }
-
-    pub fn size(&self) -> f64 {
-        self.max - self.min
-    }
-
-    pub fn contains(&self, x: f64) -> bool {
-        self.min <= x && x <= self.max
     }
 
     pub fn surrounds(&self, x: f64) -> bool {
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::rc::Rc;
 
+use rand::{Rng, rng};
+
 use crate::{
     camera::Camera,
     hit::HitWorld,
-    material::{DielectricMaterial, LambertianMaterial, MetalMaterial},
+    material::{DielectricMaterial, LambertianMaterial, Material, MetalMaterial},
     sphere::Sphere,
     vec3::Vec3,
 };
@@ -18,29 +20,62 @@ mod sphere;
 mod vec3;
 
 fn main() {
-    let mat_ground = Rc::new(LambertianMaterial::new(Vec3::new(0.8, 0.8, 0.0)));
-    let mat_center = Rc::new(LambertianMaterial::new(Vec3::new(0.1, 0.2, 0.5)));
-    let mat_left = Rc::new(DielectricMaterial::new(1.5));
-    let mat_bubble = Rc::new(DielectricMaterial::new(1.0 / 1.5));
-    let mat_right = Rc::new(MetalMaterial::new(Vec3::new(0.8, 0.6, 0.2), 1.0));
-
     let mut world = HitWorld::new();
-    world.push(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, mat_ground));
-    world.push(Sphere::new(Vec3::new(0.0, 0.0, -1.2), 0.5, mat_center));
-    world.push(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5, mat_left));
-    world.push(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.4, mat_bubble));
-    world.push(Sphere::new(Vec3::new(1.0, 0.0, -1.0), 0.5, mat_right));
+    let ground_material = Rc::new(LambertianMaterial::new(Vec3::new(0.5, 0.5, 0.5)));
+    world.push(Sphere::new(
+        Vec3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
+    ));
+
+    let mut rng = rng();
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rng.random::<f64>();
+            let center = Vec3::new(
+                a as f64 + 0.9 * rng.random::<f64>(),
+                0.2,
+                b as f64 + 0.9 * rng.random::<f64>(),
+            );
+
+            if (center - Vec3::new(4.0, 0.2, 0.0)).len() > 0.9 {
+                let sphere_material: Rc<dyn Material> = if choose_mat < 0.8 {
+                    // diffuse
+                    let albedo = Vec3::random() * Vec3::random();
+                    Rc::new(LambertianMaterial::new(albedo))
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = Vec3::random_range(0.5, 1.0);
+                    let fuzz = rng.random_range(0.0..0.5);
+                    Rc::new(MetalMaterial::new(albedo, fuzz))
+                } else {
+                    // glass
+                    Rc::new(DielectricMaterial::new(1.5))
+                };
+                world.push(Sphere::new(center, 0.2, sphere_material));
+            }
+        }
+    }
+
+    let material1 = Rc::new(DielectricMaterial::new(1.5));
+    world.push(Sphere::new(Vec3::new(0.0, 1.0, 0.0), 1.0, material1));
+
+    let material2 = Rc::new(LambertianMaterial::new(Vec3::new(0.4, 0.2, 0.1)));
+    world.push(Sphere::new(Vec3::new(-4.0, 1.0, 0.0), 1.0, material2));
+
+    let material3 = Rc::new(MetalMaterial::new(Vec3::new(0.7, 0.6, 0.5), 0.0));
+    world.push(Sphere::new(Vec3::new(4.0, 1.0, 0.0), 1.0, material3));
 
     let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
-    let samples_per_pixel = 100;
+    let image_width = 1200;
+    let samples_per_pixel = 500;
     let max_depth = 50;
     let vfov = 20.0;
-    let lookfrom = Vec3::new(-2.0, 2.0, 1.0);
-    let lookat = Vec3::new(0.0, 0.0, -1.0);
+    let lookfrom = Vec3::new(13.0, 2.0, 3.0);
+    let lookat = Vec3::new(0.0, 0.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
-    let defocus_angle = 10.0;
-    let focus_dist = 3.4;
+    let defocus_angle = 0.6;
+    let focus_dist = 10.0;
 
     let camera = Camera::new(
         image_width,
